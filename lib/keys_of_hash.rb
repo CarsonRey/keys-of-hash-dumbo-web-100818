45c492db80121c3bdfc101fc6950ea4140d6e17d
Carson Rey <c.rey494@gmail.com>
2018-09-26 01:34:33 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -13,3 +13,5 @@ end
 
 # This method should take an undefined number of arguments, using the splat operator, 
 # and return an array with every key from the hash whose value matches the value(s) given as an argument.
+
+# '.self' is a godsend! 
