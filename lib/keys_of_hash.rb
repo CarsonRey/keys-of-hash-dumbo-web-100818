63a81057d391b82b530e6760bc3d3b34263a158e
Carson Rey <c.rey494@gmail.com>
2018-09-26 01:22:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,20 +1,20 @@
 require 'pry'
 class Hash
   def keys_of(*arguments)
-    array_of_keys = []
-    if arguments.kind_of?(Array) == true
-      arguments = arguments.join
-      result = self.each do |key, value|
-                if value == arguments
-                  array_of_keys << key
-                end
-                end
-    else
-        result
-      end
-    end
-    array_of_keys
-end
+#     array_of_keys = []
+#     if arguments.kind_of?(Array) == true
+#       arguments = arguments.join
+#       result = self.each do |key, value|
+#                 if value == arguments
+#                   array_of_keys << key
+#                 end
+#                 end
+#     else
+#         result
+#       end
+#     end
+#     array_of_keys
+# end
 end
 
 # This method should take an undefined number of arguments, using the splat operator, 
